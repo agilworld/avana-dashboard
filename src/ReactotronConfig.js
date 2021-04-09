1d340b46d74912198ac6895ea38400b1619f5be7
Dian Afrial <agil.rahadi@gmail.com>
2021-04-09 17:50:21 +0700

    remove devDependecies

--- src/ReactotronConfig.js
+++
@@ -1,9 +0,0 @@
-import Reactotron from 'reactotron-react-js'
-import { reactotronRedux } from 'reactotron-redux'
-
-const reactotron = process.env.REACT_APP_ENV === 'development' ? Reactotron
-  .configure({ name: 'Avana Admin Debugging' }) // we can use plugins here -- more on this later
-  .use(reactotronRedux()) 
-  .connect() : null
-
-export default reactotron
